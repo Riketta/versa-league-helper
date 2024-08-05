3309525f824b45b069d6d1fa0e9905c50d37d65d
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-08-05 07:19:34 +0300

    Add TODO message.

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,8 @@ fn main() -> anyhow::Result<()> {
     tts.use_random_pitch(true);
     tts.use_random_voice(true);
 
+    // TODO: add a message validator to prevent messages that can be played 0 times during a game session.
+    // E.g. messages with too short an interval, placed too far down in priority.
     let messages = Message::generate_default_messages();
     let mut session = GameSession::new(&mut tts, &messages, 30, Role::Jungle);
     session.start_session_loop();
